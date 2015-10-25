c1e084fe5b85e0d26a030e3688d6469d128fdc68
Hugo <hugo.hornquist@gmail.com>
2015-10-25 17:30:26 +0100

    json added

--- src/database/DatabaseInterface.java
+++ src/database/DatabaseInterface.java
@@ -1,109 +1,69 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import gamePieces.Card;
 
 public class DatabaseInterface {
 
 	Card[] cards;
-	Card[] tempCards;
-	int noCards;
+	ArrayList<JSONObject> cardArrayList;
 
-	Connection connection = null;
-	Statement statement   = null;
-	ResultSet resultSet   = null;
-
-	//String url      = "jdbc:mysql://localhost:3306/testdb";
-	String url      = "jdbc:mysql://localhost:3306/cardlist";
-	//String user     = "testuser";
-	//String password = "test623";
-	String user     = "root";
-	String password = "";
-	
 	public DatabaseInterface(String cardList) {
 		cards = new Card[60];
 
+		ArrayList<JSONObject> cardArrayList;
 
 		try {
-			connection = DriverManager.getConnection(url, user, password);
 
-			statement = connection.createStatement();
+			JSONTokener jTokener = new JSONTokener(new FileReader("/home/hugo/code/java/cardboardGathering/database/cards.json"));
+			JSONObject obj = new JSONObject(jTokener);
 
-			String querry1 = "SELECT COUNT(*) FROM cards;";
-			resultSet = statement.executeQuery(querry1);
-			if( resultSet.next() ) {
-				noCards = resultSet.getInt("count(*)");
-				tempCards = new Card[noCards];
+			JSONArray jsonCards = obj.getJSONArray("cards");
+
+			cardArrayList = new ArrayList<JSONObject>();
+
+			for( int i = 0; i < jsonCards.length(); i++ ) {
+				cardArrayList.add(jsonCards.getJSONObject(i));
 			}
 
-			String querry2 = "SELECT * FROM cards;";
-			resultSet = statement.executeQuery(querry2);
-
-			while( resultSet.next() ) {
-				int currentRow = resultSet.getRow() - 1;
-				tempCards[currentRow] = new Card(
-					resultSet.getString("name"),
-					resultSet.getString("type"),
-					resultSet.getString("subtype"),
-					resultSet.getString("ability"),
-					resultSet.getString("flavour"),
-					resultSet.getInt("power")    ,
-					resultSet.getInt("toughness"),
-					0,
-					resultSet.getInt("manaBlack"),
-					resultSet.getInt("manaBlue") ,
-					resultSet.getInt("manaGreen"),
-					resultSet.getInt("manaRed")  ,
-					resultSet.getInt("manaWhite"),
-					resultSet.getInt("manaBlank")
-				);
+			for( JSONObject tempObj : cardArrayList ) {
+				System.out.println(tempObj.getString("name"));
 			}
+
+			Random rand = new Random();
 			for( int i = 0; i < 60; i++ ) {
-				Random rand = new Random();
-				int c = rand.nextInt(noCards);
+				int tempIndex = rand.nextInt(cardArrayList.size());
+				JSONObject tempObject = cardArrayList.get(tempIndex);
 				cards[i] = new Card(
-					tempCards[c].getCardName(),
-					tempCards[c].getType(),
-					tempCards[c].getSubtype(),
-					tempCards[c].getAbility(),
-					tempCards[c].getFlavour(),
-					tempCards[c].getPower(),
-					tempCards[c].getToughness(),
-					tempCards[c].getLoyalty(),
-					tempCards[c].getManaCostBlack(),
-					tempCards[c].getManaCostBlue(),
-					tempCards[c].getManaCostGreen(),
-					tempCards[c].getManaCostRed(),
-					tempCards[c].getManaCostWhite(),
-					tempCards[c].getManaCostBlank()
+					tempObject.getString("name"),
+					tempObject.getString("type"),
+					tempObject.getString("subtype"),
+					"This be the ability",
+					tempObject.getString("flavour"),
+					tempObject.getInt("power"),
+					tempObject.getInt("toughness"),
+					0,
+					tempObject.getInt("manaBlack"),
+					tempObject.getInt("manaBlue"),
+					tempObject.getInt("manaGreen"),
+					tempObject.getInt("manaRed"),
+					tempObject.getInt("manaWhite"),
+					tempObject.getInt("manaBlank")
 				);
 			}
-		} catch (SQLException e) {
-			Logger lgr = Logger.getLogger(DatabaseInterface.class.getName());
-			lgr.log(Level.SEVERE, e.getMessage(), e);
-		} finally {
-			try {
-				if( resultSet != null ) {
-					resultSet.close();
-				}
-				if( statement!= null ) {
-					statement.close();
-				}
-				if( connection != null ) {
-					connection.close();
-				}
-			} catch (SQLException ex) {
-				Logger lgr = Logger.getLogger(DatabaseInterface.class.getName());
-				lgr.log(Level.WARNING, ex.getMessage(), ex);
-			}
+		} catch (JSONException je) {
+			je.printStackTrace();
+		} catch (FileNotFoundException fe) {
+			fe.printStackTrace();
 		}
 
 
